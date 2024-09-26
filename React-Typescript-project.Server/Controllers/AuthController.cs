7a0b8fbc81106d35ca8590d37cd805fa8b1ba3d9
aniljangid001 <akulariya71@gmail.com>
2024-09-26 11:52:40 +0530

    minor changes

--- React-Typescript-project.Server/Controllers/AuthController.cs
+++ React-Typescript-project.Server/Controllers/AuthController.cs
@@ -2,9 +2,9 @@
 using Budget_Tracker_Bend.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
+  //using Microsoft.IdentityModel.Tokens;
 using React_Typescript_project.Server.Modals;
-using System.IdentityModel.Tokens.Jwt;
+  //using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
@@ -43,9 +43,9 @@ namespace React_Typescript_project.Server.Controllers
 
                 if (newuser != null && validPassword)
                 {
-                    var token = GenerateJwtToken(user.UserName);
+                    //  var token = GenerateJwtToken(user.UserName);
 
-                    return Ok(new {token});
+                    //  return Ok(new {token});
                 }
                 else return
                         StatusCode(404, "Un-Authrization access denied");
@@ -56,100 +56,6 @@ namespace React_Typescript_project.Server.Controllers
             }
             return Unauthorized();
         }
-
-
-     //   [HttpPost("login")]
-     //   public async Task<IActionResult> Login([FromBody] UserLogin login)
-     //   {
-     //       // Validate user credentials (e.g., check from database)
-     //       // Here, we're assuming a simple check with hardcoded values. In real apps, you'd query a database.
-     //       //if (IsValidUser
-     //       //    (login.UserName, login.Password))
-     //       //{
-     //       //    var token = GenerateJwtToken(login.UserName);
-     //       //    return Ok(new { token });
-     //       //}
-     //       try
-     //       {
-     //           var user = await _usersServices.GetUserAsync(login.UserName);
-     //
-     //           if (user != null)
-     //           {
-     //               if (login.Password == user.Password)
-     //               {
-     //                   var token = GenerateJwtToken(login.UserName);
-     //                   return Ok(new { token });
-     //               }
-     //           }
-     //       }
-     //       catch (Exception ex)
-     //       {
-     //           return Unauthorized();
-     //       }
-     //       return Unauthorized();
-     //
-     //   }
-     //
-
-
-
-        // Function to validate the username and password
-        //private bool IsValidUser(string username, string password)
-        //{
-        //    // Replace this with your own validation logic, e.g., checking against a database
-        //    // For example: return _userService.ValidateUser(username, password);
-
-        //    // Dummy validation: for now, consider username "user" and password "password123" as valid
-        //    var user = _usersServices.GetUserAsync(username);
-
-
-        //    if (user == null)
-        //    {
-        //        return true;
-        //    }
-
-        //    if ()
-        //    {
-
-        //    }
-        //    return false;
-
-        //}
-
-        // JWT token generation logic
-        private string GenerateJwtToken(string username)
-        {
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-            var claims = new[]
-            {
-            new Claim(JwtRegisteredClaimNames.Sub, username),
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-        };
-
-            var token = new JwtSecurityToken(
-                issuer: _configuration["Jwt:Issuer"],
-                audience: _configuration["Jwt:Audience"],
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(Convert.ToInt32(_configuration["Jwt:ExpiresInMinutes"])),
-                signingCredentials: credentials);
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
-
-        [Authorize]
-        [HttpGet("protected")]
-        public IActionResult Protected()
-        {
-            return Ok("This is a protected API endpoint.");
-        }
     }
-
-    //// UserLogin class to receive login details from the frontend
-    //public class UserLogin
-    //{
-    //    public string Username { get; set; }
-    //    public string Password { get; set; }
-    //}
 }
+        
