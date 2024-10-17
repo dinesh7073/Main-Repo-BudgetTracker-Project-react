a1c6bc96ec9e257805c5c2b8bd6f07d44d4fc9ed
aniljangid001 <akulariya71@gmail.com>
2024-10-17 12:07:48 +0530

    commit

--- react-typescript-project.client/src/Login-Section/Welcome.tsx
+++ react-typescript-project.client/src/Login-Section/Welcome.tsx
@@ -1,44 +1,57 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from 'react';
 import UserContext from '../UserContext';
-import { Button, Form, FormProps, Input, Segmented, Select, Space } from 'antd';
+import { Button, Form, Input, message } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { REACT_APP_BASE_URL } from '../Components/Common/Url';
+import Dashboard from '../Components/Dashboard';
 
 const Welcome = () => {
-    const { userDetails, UserId } = useContext<any>(UserContext);
-    const [inputValue, setInputValue] = useState();
+    const { userDetails, setUserDetails } = useContext<any>(UserContext); // Make sure to provide setter in context
+    const [form] = Form.useForm(); // Create form instance
     const navigate = useNavigate();
 
-    const handleChange = (e: any) => {
-        setInputValue(e.target.value);
-    }
-
-    const onSave = () => {
+    const onSave = (values: any) => {
         const account = {
             userId: userDetails.id,
             bankName: 'Cash',
             accountType: 1,
-            amount: inputValue
-        }
-        axios.post(`${REACT_APP_BASE_URL}AccountsController/${userDetails.id}CreateAccountsAndUpdate`, account).then((res) => {
-            navigate('/dashboard');
-            window.location.reload();
-            console.log(res.data);
-           
-        }).catch((err) => console.log("error", err))
+            amount: values.cashAmount, // Use form value
+        };
+        axios.post(`${REACT_APP_BASE_URL}AccountsController/${userDetails.id}CreateAccountsAndUpdate`, account)
+            .then((res) => {
+                navigate('/dashboard');
+                console.log(res.data);
+            })
+            .catch((err) => console.log("error", err));
+    };
 
-
-    }
+    const ondata = (values: any) => {
+        axios.post(`${REACT_APP_BASE_URL}UsersController/Login`, {
+            UserName: values.email,
+            password: values.password,
+        }).then((response) => {
+            if (response.data.id) {
+                localStorage.setItem('isUser', JSON.stringify({
+                    UserId: response.data.id,
+                }));
+                setUserDetails({ ...userDetails, userData: response.data });
+                navigate('/dashboard');
+                message.success(' successful!');
+            }
+        }).catch((error) => {
+            message.error('password or email is incorrect');
+            console.error('Error fetching data:', error);
+        });
+    };
 
     return (
         <div style={{ height: '' }}>
+            <p className='mt-3' onClick={() => { navigate("/dashboard"); }}>
+                <p style={{ cursor: "pointer", color: "blue", fontSize: '15.5px', marginLeft: '94%', textDecorationLine: 'underline', }}>Skip</p>
+            </p>
 
-            <p className='mt-3' onClick={() => { navigate("/dashboard"); window.location.reload(); }}><p style={{ cursor: "pointer", color: "blue", fontSize: '15.5px', marginLeft: '94%', textDecorationLine: 'underline', }}>    Skip</p> </p>
-
-            {/* <Button className=' ps-3 pe-3' onClick={() => { navigate('/dashboard'); window.location.reload(); }} style={{ marginLeft: '94%',  }}>Skip</Button> */}
             <div style={{ textAlign: 'center', marginTop: '11%' }}>
-
                 <div className="mt-4">
                     <img
                         src="https://images.pond5.com/gold-coins-cartoon-style-are-footage-276090183_iconl.jpeg"
@@ -48,55 +61,37 @@ const Welcome = () => {
                 </div>
                 <div style={{ fontFamily: 'Open Sans' }}>
                     <b>
-                        <h4 className="mt-4">Set up your cash balance</h4>
+                        <h4 className="mt-4"><b>Set up your cash balance</b></h4>
                     </b>
                 </div>
-                <div>
-                    <h6 className="mt-4" style={{ fontSize: '12px', marginLeft: '-260px' }}>
-                        Cash balance
-                    </h6>
-                    {/* <Form.Item
-                    
-                    rules={[{ required: true, message: 'Please input!' }]}
-                    
-                   style={{ width: '22%', borderRadius: '5px', marginLeft:'39%'}}>
-                     <Input   />
+                <Form form={form} onFinish={onSave}>
+                    <p  className='mt-4' style={{marginLeft:'-16.4%',}}>Cash Balance</p>
+                    <Form.Item name="bankName" rules={[{ required: true, message: 'Please input your cash amount!' },
+                        // {
+                        //     // pattern: RegExp("[1-9]{1}[0-9]{9}"),
+                        //     message: 'Invalid input',
+                        //     validateTrigger: 'onFinish',
+                            
+                        // }
 
 
-                    </Form.Item> */}
 
-                    <Form>
-                        <Form.Item name="email"
+                    ]} style={{ width: '22%', marginLeft: '39%' }}>
+                        <Input placeholder="Enter cash amount" type='number' />
+                    </Form.Item>
 
-                            rules={[{ required: true, message: 'Please input your Input!' },
-
-                            ]}
-                            style={{ width: '22%', borderRadius: '5px', marginLeft: '39%' }} >
-                            <Input />
-                        </Form.Item>
-                    </Form>
-
-                    <p className="mt-2" style={{ fontSize: '12px', marginLeft: '-60px' }}>
-                        How much cash do you have in your physical wallet?
-                    </p>
-                </div>
-                <Button type='primary'
-                    onClick={() => {
-                        navigate('/dashboard');
-                        window.location.reload();
-                        // const htmlType="submit" 
-                    }}
-                    className="mt-5"
-                    style={{ width: '23%', borderRadius: '19px', height: '37px', color: 'white',  }} >
-                    Confirm Cash Balance
-                </Button>
-
-
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        className="mt-5"
+                        style={{ width: '23%', borderRadius: '19px', height: '37px', color: 'white' }}
+                    >
+                        Confirm Cash Balance
+                    </Button>
+                </Form>
             </div>
         </div>
     );
 };
 
 export default Welcome;
-
-
